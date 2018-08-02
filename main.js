d26639b1eff66a9ed7d0e849851b83cd01e7d220
Pavel Silva <pmalave2@hotmail.com>
2018-08-02 10:02:50 -0400

    semantica en codigo, activando eslint

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-var Exercise = require('./models/testModel');
-var routes = require('./routes/testRoutes');
-const app = express();
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+var Exercise = require('./models/testModel')
+var routes = require('./routes/testRoutes')
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-routes(app);
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+routes(app)
 
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'});
-});
+app.use(function (req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+})
 
-app.get('/', function(req, res){
-   res.send("Hello world!");
-});
+app.get('/', function (req, res) {
+  res.send('Hello world!')
+})
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect('mongodb://test:12345678A@ds018308.mlab.com:18308/test_db', { useNewUrlParser: true });
 
-var server = app.listen(3001, function (){
-
-    var host = server.address().address;
-    var port = server.address().port;
- 
-    console.log("App listening at http://%s:%s", host, port);
-});
+var server = app.listen(3001, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('App listening at http://%s:%s', host, port)
+})
